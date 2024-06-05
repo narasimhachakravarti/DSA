307c0f4c35f4ce58f92f56852720481ed08ea05d
narasimhachakravarti <narasimha.s.chakravarti@gmail.com>
2024-06-05 13:05:25 +0530

    Stacks, Queues and Singly Linked List

---
+++ Queues/queue.js
@@ -0,0 +1,25 @@
+class Queue {
+    constructor() {
+        this.queue = []
+    }
+
+    enqueue(val) {
+        this.queue.push(val)
+    }
+    dequeue(val) {
+        this.queue.shift()
+    }
+
+    print() {
+        console.log(this.queue)
+    }
+}
+
+const queue = new Queue();
+queue.enqueue(5)
+queue.enqueue(34)
+queue.enqueue(3)
+queue.enqueue(1)
+queue.print()
+queue.dequeue()
+queue.print()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DSA
+### Time Complexity
+
+**Push** O(1)
+**Pop** O(1)
+**Print** O(n)
